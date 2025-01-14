379822299bce36ff83cbc7b510c2c9b41ed03c2b
Celson Paixão <celson.paixao@mamboo.co.ao>
2025-01-14 17:53:53 +0100

    add Model

--- app/src/main/java/com/celsonpaixao/appbebergua/MainActivity.kt
+++ app/src/main/java/com/celsonpaixao/appbebergua/MainActivity.kt
@@ -1,20 +1,77 @@
 package com.celsonpaixao.appbebergua
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.celsonpaixao.appbebergua.model.CalcularModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editPeso: EditText
+    private lateinit var editIdade: EditText
+    private lateinit var btCalcular: Button
+    private lateinit var textResultado: TextView
+    private lateinit var btRefresh: ImageView
+
+    private lateinit var caluclarModel: CalcularModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        initializaComponent()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        btCalcular.setOnClickListener {
+            if (editPeso.text.toString().isEmpty()) {
+                Toast.makeText(this, R.string.alert_peso_void, Toast.LENGTH_SHORT).show()
+            } else if (editIdade.text.toString().isEmpty()) {
+                Toast.makeText(this, R.string.alert_idade_void, Toast.LENGTH_SHORT).show()
+            } else {
+                //  editIdade.text.clear()
+                //  editPeso.text.clear()
+
+            }
+        }
+    }
+
+    private fun initializaComponent() {
+        caluclarModel = CalcularModel()
+        editIdade = findViewById(R.id.input_idade)
+        editPeso = findViewById(R.id.input_peso)
+        btCalcular = findViewById(R.id.bt_calcular)
+        textResultado = findViewById(R.id.text_result_ml)
+        btRefresh = findViewById(R.id.ic_refresh)
+
+    }
+
+    private fun calculeMl(peso: Double, idade: Double): String {
+
+        var result: String = ""
+
+        var jovemML = 40
+        var adultoML = 35
+        var idosoML = 30
+
+
+
+
+        return result
     }
 } 
---
+++ app/src/main/java/com/celsonpaixao/appbebergua/model/Calcular.kt
@@ -0,0 +1,13 @@
+package com.celsonpaixao.appbebergua.model
+
+class CalcularModel {
+    private var jovemMl = 40
+    private var adultMl = 35
+    private var idosoMl = 30
+    private var semiMortoMl = 25
+
+
+    private fun calculeResultML(peso: Double, idade: Double): String{
+
+    }
+}
